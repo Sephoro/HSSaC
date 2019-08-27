a64aa3c5078d372e27d20430a77bc2195dd10699
Boikanyo13 <bv.radiokana@gmail.com>
2019-08-27 04:29:19 +0200

    Modified GUI plots

--- Code/Segmentation/FFTPlot.m
+++ Code/Segmentation/FFTPlot.m
@@ -10,8 +10,8 @@ set(gcf,'Position',[100 100 571, 251]);
 
 set(gcf, 'Color', 'None');
 set(gca,'Color','None');
-set(gca,'xcolor','w') % Set the x axis color to white
-set(gca,'ycolor','w') % Set the y axis color to white
+set(gca,'xcolor','k') % Set the x axis color to white
+set(gca,'ycolor','k') % Set the y axis color to white
 
 addpath('./export_fig_folder/');
 save_path = strcat('../Figures/', name);
