dcd2426d7fc013893251f1d886528d228b35ae13
sephoro <eliassepuru@gmail.com>
2019-08-28 05:10:38 +0200

    Made a function

--- Code/Segmentation/getFeatureVector.py
+++ Code/Segmentation/getFeatureVector.py
@@ -1,7 +1,10 @@
-import numpy as np # Tradition
 import matlab.engine
 
 
-eng = matlab.engine.start_matlab()
-tf = eng.GenerateFeatures('../../../../Dataset/setA/Atraining_normal/Atraining_normal/201108011114.wav')
-print(tf)
+def getFeatures(filename):
+    
+    eng = matlab.engine.start_matlab()
+    features = eng.GenerateFeatures(filename)
+    return features
+
+print(getFeatures('../../../../Dataset/setA/Atraining_normal/Atraining_normal/201108011114.wav'))
