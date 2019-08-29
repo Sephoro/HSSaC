ae3a5e8bc6dd9f7957f040a3093693c72ccb8ad6
sephoro <eliassepuru@gmail.com>
2019-08-29 04:04:55 +0200

    Implemented classify member function of Classifier class

--- Code/Segmentation/classifier.py
+++ Code/Segmentation/classifier.py
@@ -1,6 +1,8 @@
 import sys
+import numpy as np
 from FeatureVector import generateFeatures
 sys.path.append('../Classification')
+import models as md # Models to be used
 
 
 class Classifier:
@@ -13,7 +15,37 @@ class Classifier:
 
         self.featureVector = generateFeatures(self.filedir,self.datasetDir)
 
-        return self.featureVector
+        # return self.featureVector
+    
+    def classify(self):
+        
+        # Classes to be predicted
+
+        classes = np.array(['Extrasystole', 'Murmur', 'Normal'])
+
+        # Classify the heartsound
+        if self.modelName == 'ANN':
+            ANN = md.ANN()
+            result = ANN.predict_proba(self.featureVector)
+            print(result)
+
+        elif self.modelName == 'XGB':
+
+            XGB = md.XGB()
+            result = XGB.predict_proba(self.featureVector)
+
+        elif self.modelName == 'SVM':
+
+            SVM = md.SVM()
+            result = SVM.predict_proba(self.featureVector)
+            print(result)
+    
+        confidence = max(result[0])
+        class_ = (classes[np.where(result[0] ==  confidence)])[0]
+        
+        return class_ , confidence*100
+        
+
     
 
 
@@ -21,8 +53,7 @@ class Classifier:
 
 f = '../../../../Dataset/setA/Atraining_normal/Atraining_normal/201108011114.wav'
 
-c = Classifier('ANN', f)
+c = Classifier('SVM', f)
 
-X = c.getFeatures()
-
-print(X)
+c.getFeatures()
+c.classify()
