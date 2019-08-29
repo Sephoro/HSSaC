7a64024d3657672436e302ccd46bad9954725201
sephoro <eliassepuru@gmail.com>
2019-08-29 02:25:01 +0200

    Cleaned getFeaturevector.py

--- Code/Segmentation/getFeatureVector.py
+++ Code/Segmentation/getFeatureVector.py
@@ -22,7 +22,7 @@ def getOriginalInputs(filename):
     df = df.iloc[:, 0:toremove]
     
     # Exclude the labels, not necessary
-    X = df.iloc[0:3,0:26]
+    X = df.iloc[:,0:26]
 
     return X
 
@@ -31,24 +31,9 @@ def generateFeatures(filename, dataset):
     # Normalise and return ready for classification
     a = getRawFeatures(filename)
     b = getOriginalInputs(dataset).values
-    # print(a)
-    # print(b)
-    # Concate and ready for normalisation
     b = np.concatenate((a,b))
-    # print('\n\n')
-    # print(b)
-    # Normalise
     min_max_scaler = MinMaxScaler()
     x_scaled = min_max_scaler.fit_transform(b)
     x = pd.DataFrame(x_scaled)
-    
-    # The top one is the one
+
     return x.iloc[0:1,0:26]
-
-
-
-f = '../../../../Dataset/setA/Atraining_normal/Atraining_normal/201108011114.wav'
-d = '../Dataset/heartbeatFeaturesB4.csv'
-
-X = generateFeatures(f,d)
-print(X)
