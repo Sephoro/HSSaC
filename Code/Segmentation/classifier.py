c7be0737ef99a20eea29aa6ca4bb741b654c7576
sephoro <eliassepuru@gmail.com>
2019-08-29 04:19:37 +0200

    Modified the Classifier Class

--- Code/Segmentation/classifier.py
+++ Code/Segmentation/classifier.py
@@ -6,19 +6,22 @@ import models as md # Models to be used
 
 
 class Classifier:
-    def __init__(self, modelName, filedir):
-        self.modelName = modelName
-        self.filedir = filedir
+    def __init__(self):
+       
         self.datasetDir = '../Dataset/heartbeatFeaturesB4.csv'
     
-    def getFeatures(self):
+    def getFeatures(self, filedir):
+
+        self.filedir = filedir
 
         self.featureVector = generateFeatures(self.filedir,self.datasetDir)
 
         # return self.featureVector
     
-    def classify(self):
+    def classify(self,modelName):
         
+        self.modelName = modelName
+
         # Classes to be predicted
 
         classes = np.array(['Extrasystole', 'Murmur', 'Normal'])
@@ -42,18 +45,5 @@ class Classifier:
     
         confidence = max(result[0])
         class_ = (classes[np.where(result[0] ==  confidence)])[0]
-        
+
         return class_ , confidence*100
-        
-
-    
-
-
-
-
-f = '../../../../Dataset/setA/Atraining_normal/Atraining_normal/201108011114.wav'
-
-c = Classifier('SVM', f)
-
-c.getFeatures()
-c.classify()
