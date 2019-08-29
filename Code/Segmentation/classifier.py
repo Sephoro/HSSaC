80505255fcefa7e167d7716c957f6bd402a8a622
sephoro <eliassepuru@gmail.com>
2019-08-29 03:38:50 +0200

    Created Classifier class and getFeatures member function

--- Code/Segmentation/classifier.py
+++ Code/Segmentation/classifier.py
@@ -3,9 +3,26 @@ from FeatureVector import generateFeatures
 sys.path.append('../Classification')
 
 
-f = '../../../../Dataset/setA/Atraining_normal/Atraining_normal/201108011114.wav'
-d = '../Dataset/heartbeatFeaturesB4.csv'
+class Classifier:
+    def __init__(self, modelName, filedir):
+        self.modelName = modelName
+        self.filedir = filedir
+        self.datasetDir = '../Dataset/heartbeatFeaturesB4.csv'
+    
+    def getFeatures(self):
 
-X = generateFeatures(f,d)
+        self.featureVector = generateFeatures(self.filedir,self.datasetDir)
+
+        return self.featureVector
+    
+
+
+
+
+f = '../../../../Dataset/setA/Atraining_normal/Atraining_normal/201108011114.wav'
+
+c = Classifier('ANN', f)
+
+X = c.getFeatures()
 
 print(X)
