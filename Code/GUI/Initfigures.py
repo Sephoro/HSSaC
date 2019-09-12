2262ba58f86af05d12f8eb0207540784b342ba3e
sephoro <eliassepuru@gmail.com>
2019-09-12 03:22:19 +0200

    Moved InitFigures to GUI folder

--- Code/GUI/Initfigures.py
+++ Code/GUI/Initfigures.py
@@ -9,4 +9,8 @@ def generateInitFigures(filename):
     
     # Generate initial figures
     eng = matlab.engine.start_matlab()
+    eng.cd(r'../Segmentation')
     eng.InitialPlots(filename,nargout=0) 
+
+
+generateInitFigures('/home/elias/Dropbox/YOS4/ELEN4012/Dataset/setA/Aunlabelledtest/Aunlabelledtest/201101152256.wav')
